8984d771ce6f3ffe6713df4bc69c2adbab951df7
Jose Garcia <jgcordoba@gmail.com>
2022-12-10 18:12:59 +0100

    canvasReload

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField] Image aro;
+
     public bool gun;
 
     public int bullets=6;
     
     void Start()
     {
+       
         
     }
 
     
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+           if (gun==false && bullets==0)
+             {
+            aro.enabled = true;
+           }
+             else
+             {
+            aro.enabled = false;
+            }
+
+        }
+        
+
+
+
+    
         
     }
 }
